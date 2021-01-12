a8201650bf641bba57ab20f51674e6f4fbc715d8
Marcelo Leivas <marceloleivas@ip-192-168-100-21.us-west-2.compute.internal>
2021-01-12 20:08:04 -0300

    adding login and principal page classes

---
+++ e2e_tests/pages/principal_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from .po.page_objects import Page
+
+
+class LoginPage(Page):
+
+    def header_title(self):
+        return self.wait.until(
+            EC.presence_of_element_located((By.ID, 'logo-container'))
+        )
+
+    def page_title(self):
+        return self.wait.until(
+            EC.presence_of_element_located((By.ID, 'Lista de Produtos'))
+        )
+
+    def add_product_bttn(self):
+        return self.wait.until(
+            EC.presence_of_element_located((By.LINK_TEXT, 'ADICIONAR PRODUTOS'))
+        )
